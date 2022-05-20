7b0771d043b91da38a7365c83db4c41a5c3a05a5
April Reitan <45362322+areitan@users.noreply.github.com>
2022-05-19 19:17:51 -0700

    adding Product.js columns

--- models/Product.js
+++ models/Product.js
@@ -1,59 +1,52 @@
-// `Product`
-
-//   * `id`
-  
-//         * Integer.
-  
-//         * Doesn't allow null values.
-  
-//         * Set as primary key.
-  
-//         * Uses auto increment.
-
-//   * `product_name`
-  
-//         * String.
-  
-//         * Doesn't allow null values.
-
-//   * `price`
-  
-//         * Decimal.
-  
-//         * Doesn't allow null values.
-  
-//         * Validates that the value is a decimal.
-
-//   * `stock`
-  
-//         * Integer.
-  
-//         * Doesn't allow null values.
-  
-//         * Set a default value of `10`.
-  
-//         * Validates that the value is numeric.
-
-//   * `category_id`
-  
-//         * Integer.
-  
-//         * References the `Category` model's `id`.
-
-
-
 // import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    //   * `id`is an Integer that Doesn't allow null values is Set as primary key and Uses auto increment.
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    //   * `product_name` is a String that Doesn't allow null values.
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    //   * `price`is a Decimal that Doesn't allow null values and Validates that the value is a decimal.
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    //   * `stock`is an Integer that Doesn't allow null values with a Set default value of `10` and Validates that the value is numeric.
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    //   * `category_id`is an Integer that References the `Category` model's `id`.
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
