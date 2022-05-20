9a6215bd59e532eddd5f3dc2909495ea532f584f
April Reitan <45362322+areitan@users.noreply.github.com>
2022-05-19 18:51:48 -0700

    adding Tag.js columns

--- models/Tag.js
+++ models/Tag.js
@@ -1,20 +1,3 @@
-// `Tag`
-
-//   * `id`
-  
-//         * Integer.
-  
-//         * Doesn't allow null values.
-  
-//         * Set as primary key.
-  
-//         * Uses auto increment.
-
-//   * `tag_name`
-  
-//         * String.
-
-
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
@@ -24,6 +7,25 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    //   * `id is an Integer that Doesn't allow null values is Set as primary key that Uses auto increment.
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // * `product_id` is an Integer that References the `Product` model's `id`.
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    //   * `tag_name` is a String.
+    tag_name: {
+      type: DataTypes.STRING,
+        },
   },
   {
     sequelize,
